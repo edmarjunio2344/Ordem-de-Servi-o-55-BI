aec450033d03c0435692b3b6f0b16df6287fd02c
unknown <you@example.comedmarjunio6@gmail.com>
2023-11-26 21:15:18 -0300

    primeiro commit

---
+++ Config mysql.php
@@ -0,0 +1,18 @@
+<?php
+
+$dbHost = `localhost`;
+$dbUsername = `root`;
+$dbPassword = '';
+$dbName = `formulario55°bi`;
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+if($conexao->connect_errno)
+{
+    echo "Erro";
+}
+else
+{
+    echo "Conexão efetuada com sucesso"
+}
+?>
